61e42a7dd71b7c36ee8d4a8e914fed2056fe4fba
Gülçin Düzgün <glcndzgn14@gmail.com>
2018-04-24 21:17:18 +0300

    butona sıfırlama aksiyonu verirken geçen süreyi konsola yazdırma komutu verildi

--- src/main/java/widgets/WorkTimer.java
+++ src/main/java/widgets/WorkTimer.java
@@ -1,16 +1,25 @@
 package widgets;
 
+import util.ImageLoader;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.LinkedList;
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 public class WorkTimer extends Widget{
 
     private JLabel  label;
     private JButton button;
-    private int buttonstate = 0;
-    private int currentTime;
+    private ImageIcon workIcon;
     private Timer   timer;
+    private Label workLabel;
+    private int currentTime;
+    private int buttonState = 0;
+    public LinkedList<String> recordedTimes = new LinkedList<String>();
+
 
     public WorkTimer () {
         super(WidgetType.WORKTIMER);
@@ -19,26 +28,37 @@ public class WorkTimer extends Widget{
     }
 
     private void init() {
+
+        try {
+            workIcon = new ImageIcon(new ImageLoader().loadImage("icon_work.png", 40, 40));
+        } catch (Exception e) {
+            LOGGER.warning("Chronometer button icons could not be loaded.");
+        }
+
         label = new JLabel();
         label.setSize(150, 50);
         label.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
 
+        workLabel = new Label();
+        workLabel.setSize(100,50);
+
+
         button = new JButton();
         button.setSize(50, 50);
-        button.addActionListener(e -> {
-            if (buttonstate == 0){
+        button.addActionListener((ActionEvent e) -> {
+            if (buttonState == 0) {
                 play();
-            }else
-                stop();
+                recordedTimes.add("geçen süre:"+ recordedTimes);
+                System.out.println(recordedTimes);
+            } else
+                reset();
         });
-
-            timer = new Timer(50, e->{
-                currentTime += 50;
-                label.setText(formatTime(currentTime));
-            });
-
+        button.setIcon(workIcon);
+        timer = new Timer(50, e -> {
+            currentTime += 50;
+            label.setText(formatTime(currentTime));
+        });
     }
-
     private String formatTime(int millis){
 
         int seconds = millis  /1000;
@@ -56,32 +76,39 @@ public class WorkTimer extends Widget{
     }
 
 
-
     private void play(){
         timer.start();
-        buttonstate = 1;
-    };
+        buttonState = 1;
+    }
 
     private void stop(){
         timer.stop();
-        buttonstate = 0;
+        buttonState = 0;
+    }
+
+    private void reset(){
+        currentTime = 0;
+        recordedTimes.add(label.getText());
+        label.setText("0.0.0.00");
+        stop();
+
+        for(int i = 0 ; i < recordedTimes.size(); i ++){
+            String str = recordedTimes.get(i);
+            System.out.println(str);
+        }
+
     }
 
 
-
-
-
-    @Override
     public void setPosition(int x, int y) {
         label.setLocation(x,y);
         button.setLocation(label.getWidth()+x,y);
-
+        workLabel.setLocation(x,label.getHeight()+y);
     }
-
     @Override
     public void addToFrame(JFrame frame) {
         frame.add(label);
         frame.add(button);
-
+        frame.add(workLabel);
     }
 }
